905962af8d57d799fc8ff7ae01071f46e7eb562e
AlialhadiAbokhalil <166250920+AliAlhadiAk@users.noreply.github.com>
2024-07-18 15:31:20 +0300

    Update EccomerceTesting.cs

--- TestingWebApi/EccomerceTesting.cs
+++ TestingWebApi/EccomerceTesting.cs
@@ -1,4 +1,4 @@
-﻿using FluentAssertions;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Org.BouncyCastle.Asn1.Cms;
@@ -15,6 +15,10 @@ namespace TestingWebApi
 {
     public class EccomerceTesting
     {
+        private EccomerceTesting _controller;
+        private Mock<IProductService> _mockProductService;
+
+    
         [Fact]
         public async Task OnFetchEmptyListReturnNotFouund()
         {
@@ -94,8 +98,75 @@ namespace TestingWebApi
             GetCart.Should().NotBeNull();
             GetCart.Should().BeOfType<OkObjectResult>();
             GetCart.Value.Should().NotBeNull();
-            GetCart.Value.Should().BeOfType<Products>();
+            GetCart.Value.Should().BeOfType<Task<List<Products>>>();
 
         }
+
+        [Fact]
+        public async Task OnFailurePurchaseReturn400Badrequest()
+        {
+            var mockService = new Mock<IProductService>();
+            mockService.Setup(service => service.Purchase(It.IsAny<Guid>(), It.IsAny<int>()))
+               .ReturnsAsync(false);
+
+            var controller = new EccomerceController(mockService.Object);
+            var Purchase = (NotFoundObjectResult)await controller.Purchase("2a7eaf6d-89e0-4d9c-af66-4a6f5b5b7b1c", 1);
+            mockService.Verify(service => service.Purchase(It.IsAny<Guid>(),It.IsAny<int>()), Times.Once);
+            Purchase.Should().BeOfType<NotFoundObjectResult>();
+
+        }
+        [Fact]
+        public async Task AddBookAdmin()
+        {
+            var product = new Products
+            {
+                Id = 2,
+                Name = "Jeans",
+                Description = "Slim-fit denim jeans",
+                ImageUrl = "https://example.com/jeans.jpg",
+                price = 39.99
+
+            };
+            var mockService = new Mock<IProductService>();
+            mockService.Setup(service => service.addProductAdmin(product))
+               .ReturnsAsync(true);
+
+            var controller = new EccomerceController(mockService.Object);
+            var Purchase = (OkObjectResult)await controller.AddProductAdmin(product);
+            mockService.Verify(service => service.addProductAdmin(It.IsAny<Products>()), Times.Once);
+            Purchase.Should().BeOfType<OkObjectResult>();
+        }
+        [Fact]
+        public async Task OnFAilingReturn400_BadRequest()
+        {
+            int product_Id = 999;
+            var mockService = new Mock<IProductService>();
+            mockService.Setup(service => service.RemoveProductAdmin(product_Id))
+               .ReturnsAsync(null);
+            var controller = new EccomerceController(mockService.Object);
+            var Purchase = await controller.DeletProducAdmin(product_Id);
+            mockService.Verify(service => service.addProductAdmin(It.IsAny<Products>()), Times.Once);
+            Purchase.Should().BeOfType<NotFoundObjectResult>();
+        }
+        [Fact]
+        public async Task GetOrderHistory_Returns_OkObjectResult_With_Valid_Data()
+        {
+        
+            Guid userId = Guid.NewGuid();
+            var orderHistoryData = new object();
+            var mockService = new Mock<IProductService>();
+            mockService.Setup(x => x.UserOrderHistory(Guid.NewGuid))
+                               .ReturnsAsync(orderHistoryData);
+
+            // Act
+            var controller = new EccomerceController(mockService.Object);
+            var result = await controller.GetOrderHistory(Guid.NewGuid)
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+            var okResult = result as OkObjectResult;
+            Assert.Equal(orderHistoryData, okResult.Value);
+        }
+
     }
 }
